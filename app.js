03e9ba8c177c7c6ce5824449bfbabb6c003bccce
elhabal <ali.mahmoud.elhabal@gmail.com>
2024-08-03 16:52:19 +0300

    commit

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.send('Hello World!llll')
 })
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
+mongoose
+.connect("mongodb+srv://Elhabal:a1a2a3a4a5@cluster0.wnmcdi9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/`)
+      })
 })
+.catch((err) => {console.log(err)});
